c75245a5f8a12e8ca18ed076f063dd017e4d7c18
Hazem Issa <hazemt61@gmail.com>
2025-03-11 09:58:38 +0900

    tail recursion

--- Math.fs
+++ Math.fs
@@ -5,7 +5,9 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
-let rec collatz (n: int) : int =
-  if n = 1 then 0
-  elif n % 2 = 0 then 1 + collatz (n / 2)
-  else 1 + collatz (3 * n + 1)
+let collatz (n: int) : int =
+    let rec tail (n: int) (steps: int) : int =
+        if n = 1 then steps
+        elif n % 2 = 0 then tail (n / 2) (steps + 1)
+        else tail (3 * n + 1) (steps + 1)
+    tail n 0
