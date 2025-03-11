e8d2104d2d3ecc78005b9818fa6ad45a7f442e2b
Hazem Issa <hazemt61@gmail.com>
2025-03-11 09:55:08 +0900

    regular recursion

--- Math.fs
+++ Math.fs
@@ -5,5 +5,7 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
-let collatz n =
-  0
+let rec collatz (n: int) : int =
+  if n = 1 then 0
+  elif n % 2 = 0 then 1 + collatz (n / 2)
+  else 1 + collatz (3 * n + 1)
